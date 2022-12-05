40d3dca2e49bdcdc529ca8d0b52aed4bdce036e3
unional <homawong@gmail.com>
2022-12-04 17:27:18 -0800

    test: remove console logs

--- scenarios/ts-esm/jest.config.mjs
+++ scenarios/ts-esm/jest.config.mjs
@@ -1,13 +1,8 @@
 import { withChalk } from '@repobuddy/jest'
 import preset from '@repobuddy/jest/presets/ts-esm'
 
-console.log('preset', preset)
-console.log(`withChalk(preset)`, withChalk(preset))
-
 /** @type {import('jest').Config} */
 export default {
   ...withChalk(preset),
-  // transform: preset.transform,
-  // preset: '@repobuddy/jest/presets/ts-esm',
   roots: ['<rootDir>/ts']
 }
