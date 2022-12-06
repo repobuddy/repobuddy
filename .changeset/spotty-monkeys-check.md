c88b8d08c365ad03e465a09a28b2fd6d107dbb53
unional <homawong@gmail.com>
2022-12-06 00:23:30 -0800

    feat: add `configSrcDir()`

---
+++ .changeset/spotty-monkeys-check.md
@@ -0,0 +1,6 @@
+---
+'@repobuddy/jest': minor
+---
+
+add `configSourceDir()`.
+add `configNodejs()` as alias of `createNodejsConfig()`.
--- packages/jest/ts/config.spec.ts
+++ packages/jest/ts/config.spec.ts
@@ -1,10 +1,21 @@
 import { describe, it } from '@jest/globals'
 import { a } from 'assertron'
 import { every } from 'satisfier'
-import { createNodejsConfig } from './index.js'
+import { configNodejs, configSourceDir } from './index.js'
 
-describe(`${createNodejsConfig.name}()`, () => {
+describe(`${configNodejs.name}()`, () => {
   it('defaults with spec|test|unit|accept|integrate|system identifiers', () => {
-    a.satisfies(createNodejsConfig().testRegex, every(/\(spec|test|unit|accept|integrate|system\)/))
+    a.satisfies(configNodejs().testRegex, every(/\(spec|test|unit|accept|integrate|system\)/))
+  })
+})
+
+describe(`${configSourceDir.name}()`, () => {
+  it('can specify different root', () => {
+    expect(configSourceDir('ts')).toEqual({
+      collectCoverageFrom: [
+        '<rootDir>/ts/**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}'
+      ],
+      roots: ['<rootDir>/ts']
+    })
   })
 })
