3992b7792a2c6ee71ad55512dcb2bee3f4d3e481
unional <homawong@gmail.com>
2022-12-09 23:33:53 -0800

    feat: remove `withChalk()`

--- packages/jest/ts/index.ts
+++ packages/jest/ts/index.ts
@@ -1,5 +1,4 @@
 export * from './config.js'
 export * from './presets/index.js'
 export { sync as resolver } from './resolver.js'
-export * from './withChalk.js'
 export * from './withTransformEsmPackages.js'
