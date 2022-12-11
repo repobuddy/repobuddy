2612408dd424452983d44fba2e25f4d8f63605b4
unional <homawong@gmail.com>
2022-12-10 16:32:27 -0800

    test: improve coverage
    resolver coverage is skipped as too troubesome to test.
    It is covered by actual testcases

---
+++ packages/jest/ts/configs/configSource.ctx.ts
@@ -0,0 +1,5 @@
+import { existsSync } from 'node:fs'
+
+export const ctx = {
+  existsSync
+}
--- packages/jest/ts/matchers/toSatisfies.spec.ts
+++ packages/jest/ts/matchers/toSatisfies.spec.ts
@@ -1,23 +1,23 @@
 import { expect, it } from '@jest/globals'
 import { a } from 'assertron'
 import stripAnsi from 'strip-ansi'
-import { toSatisfies } from './toSatisfies.js'
+import { toSatisfies } from './index.js'
 
 expect.extend({ toSatisfies })
 
 it('adds to expect', () => {
-  expect({ a: 1 }).toSatisfies({ a: x => x === 1 })
+  expect({ a: 1 }).toSatisfies({ a: (x) => x === 1 })
 })
 
 it('prints fail expectation', () => {
-  const err = a.throws(() => expect({ a: 1 }).toSatisfies({ a: x => x === 2 }))
+  const err = a.throws(() => expect({ a: 1 }).toSatisfies({ a: (x) => x === 2 }))
   expect(stripAnsi(err.message)).toEqual(`expect(received).toSatisfies()
 
 expect 'a' to satisfy x => x === 2, but received 1`)
 })
 
 it('prints negative fail expectation', () => {
-  const err = a.throws(() => expect({ a: 1 }).not.toSatisfies({ a: x => x === 1 }))
+  const err = a.throws(() => expect({ a: 1 }).not.toSatisfies({ a: (x) => x === 1 }))
   expect(stripAnsi(err.message)).toEqual(`expect(received).not.toSatisfies()
 
 received {"a": 1}`)
