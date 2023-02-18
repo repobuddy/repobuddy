e08a0e79cbef72f09e8cebec073c4d0242a91629
Homa Wong <homawong@gmail.com>
2023-02-17 21:18:40 -0800

    Non-dot (#68)
    * fix: exclude files with non-dot as tests
    
    e.g. `a_spec.js` or `b_test.ts`
    
    * fix: exclude files with non-dot as tests
    
    e.g. `a_spec.js` or `b_test.ts`

--- packages/jest/ts/configs/electron.ts
+++ packages/jest/ts/configs/electron.ts
@@ -4,11 +4,11 @@ import { knownRunners, knownTestEnvironments } from '../fields/index.js'
 export const electron = {
   runner: knownRunners.electron,
   testEnvironment: knownTestEnvironments.node,
-  testMatch: ['**/?*.(spec|test|unit|accept|integrate|system)?(.electron).(js|jsx|cjs|mjs|ts|tsx|cts|mts)']
+  testMatch: ['**/?*\\.(spec|test|unit|accept|integrate|system)?(.electron).(js|jsx|cjs|mjs|ts|tsx|cts|mts)']
 } satisfies Config
 
 export const electronRenderer = {
   runner: knownRunners.electronRenderer,
   testEnvironment: knownTestEnvironments.electron,
-  testMatch: ['**/?*.(spec|test|unit|accept|integrate|system).(js|jsx|cjs|mjs|ts|tsx|cts|mts)']
+  testMatch: ['**/?*\\.(spec|test|unit|accept|integrate|system).(js|jsx|cjs|mjs|ts|tsx|cts|mts)']
 } satisfies Config
---
+++ testcases/ts-cjs/src/not_a_spec.ts
@@ -0,0 +1,1 @@
+console.log('this is not a spec file')
