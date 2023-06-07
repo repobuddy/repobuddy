f2ed32c7680fd1994ed5a0e939f34e5e327f0eaf
Homa Wong <homawong@gmail.com>
2023-06-06 20:25:48 -0700

    chore: lint

--- testcases/jsdom-formatjs/jest.config.mjs
+++ testcases/jsdom-formatjs/jest.config.mjs
@@ -2,15 +2,14 @@
 export default {
 	extensionsToTreatAsEsm: ['.ts'],
 	transform: {
-		"^.+\\.(ts|tsx|cts|mts)$": [
-			"ts-jest", {
-				"isolatedModules": true,
-            "useESM": true,
-            "diagnostics": {
-              "ignoreCodes": [
-                151001
-              ]
-            }
+		'^.+\\.(ts|tsx|cts|mts)$': [
+			'ts-jest',
+			{
+				isolatedModules: true,
+				useESM: true,
+				diagnostics: {
+					ignoreCodes: [151001]
+				}
 			}
 		]
 	}
