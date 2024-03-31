55fc2f9aa1d17a8c5a83cb1e2a26a10f0d5a469c
unional <homawong@gmail.com>
2024-03-30 23:22:00 -0700

    chore: adjust biome tsconfig

--- packages/biome/src/correctness/no_unused_variables.error.js
+++
@@ -1,5 +0,0 @@
-// biome-ignore lint/correctness/noUnusedVariables: expected error
-const unusedVar = 0
-
-// biome-ignore lint/correctness/noUnusedVariables: expected error
-export function foo(unused) {}
