a54ae1cfee4d49d63bc572432d996b44f4be4499
Homa Wong <homawong@gmail.com>
2024-04-12 16:49:33 -0700

    feat(biome): remove trailingComma: none

---
+++ .changeset/proud-files-own.md
@@ -0,0 +1,9 @@
+---
+'@repobuddy/biome': minor
+---
+
+Remove `trailingComma: none` recommendation.
+
+Following "least friction" rule,
+using the default `trailingComma: all` is better than `trailingComma: none`.
+It is easier to edit and cleaner git history.
---
+++ packages/biome/src/formatter/tailing_comma.ts
@@ -0,0 +1,15 @@
+export type Foo = {
+	a: string
+	b: string
+}
+
+export function foo(
+	apple: number,
+	banana: number,
+	cantalope: number,
+	dingle: number,
+	espresso: number,
+	funny_joke: number,
+) {
+	return apple + banana + cantalope + dingle + espresso + funny_joke
+}
