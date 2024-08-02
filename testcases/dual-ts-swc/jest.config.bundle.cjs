f0f69e5b3e6f1e0cf253413da7290fd8053c00f8
unional <homawong@gmail.com>
2024-08-02 00:02:03 -0700

    chore: clean up

--- testcases/dual-ts-swc/jest.config.bundle.cjs
+++ testcases/dual-ts-swc/jest.config.bundle.cjs
@@ -1,12 +1,10 @@
-const { fields } = require('@repobuddy/jest');
-
 /** @type {import('jest').Config} */
 const config = {
-  preset: '@repobuddy/jest/presets/jsdom-ts',
-  detectOpenHandles: true,
-	moduleNameMapper:{
-		'^uuid$': require.resolve('uuid')
-	}
+	preset: '@repobuddy/jest/presets/jsdom-ts',
+	detectOpenHandles: true,
+	moduleNameMapper: {
+		'^uuid$': require.resolve('uuid'),
+	},
 }
 
 module.exports = config
