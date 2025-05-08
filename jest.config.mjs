55b9d2e300b602209216e628698b153cbf2f7250
Homa Wong <hwong@paloaltonetworks.com>
2025-05-07 18:20:31 -0700

    chore: remove top-level jest
    This repo will have multiple test runners.
    Thus having a top-level test runner doesn't make sense anymore.

--- jest.config.mjs
+++
@@ -1,9 +0,0 @@
-// this does not work because the presets get the root dir as the cwd.
-// https://github.com/facebook/jest/issues/9623
-
-/** @type {import('jest').Config} */
-const config = {
-	projects: ['<rootDir>/packages/*'],
-}
-
-export default config
