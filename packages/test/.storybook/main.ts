dce7096df2a1fd5da0b312e3e7298bda0da59135
unional <homawong@gmail.com>
2025-12-07 02:00:33 -0800

    chore(deps): update storybook and related dependencies to v10.1.4

--- packages/test/.storybook/main.ts
+++ packages/test/.storybook/main.ts
@@ -1,5 +1,9 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
 import type { StorybookConfig } from '@storybook/react-vite'
+import { createRequire } from 'node:module'
 import { dirname, join } from 'node:path'
+
+const require = createRequire(import.meta.url)
 
 /**
  * This function is used to resolve the absolute path of a package.
